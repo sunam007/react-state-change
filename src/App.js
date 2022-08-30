8ea33c724d8b69d73813df98e55cf0d1923c2950
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-08-31 01:22:23 +0600

    load dynamic data using fetch-then and display data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+    </div>
+  );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h3>External Users:</h3>
+      <p>{users.length}</p>
+      {users.map((user) => (
+        <User name={user.name} email={user.email}></User>
+      ))}
+    </div>
+  );
+}
+
+function User(props) {
+  const userStyle = {
+    color: "lightcoral",
+    border: "1px solid lightcoral",
+    borderRadius: "10px",
+    backgroundColor: "lightblue",
+    margin: "10px",
+    padding: "10px",
+  };
+  return (
+    <div style={userStyle}>
+      <h3>Name: {props.name}</h3>
+      <h4>Email: {props.email}</h4>
     </div>
   );
 }
